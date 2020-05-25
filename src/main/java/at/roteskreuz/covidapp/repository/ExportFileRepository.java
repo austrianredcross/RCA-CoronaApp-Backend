6b231e216ca08565ba6ae85d665b1ed0164770b1
Bernhard Roessler <bernhard.roessler@accenture.com>
2020-05-25 17:30:56 +0200

    https://tasks.pxp-x.com/browse/CTAA-1469: Backend - Implement cleanup

--- src/main/java/at/roteskreuz/covidapp/repository/ExportFileRepository.java
+++ src/main/java/at/roteskreuz/covidapp/repository/ExportFileRepository.java
@@ -1,7 +1,11 @@
 package at.roteskreuz.covidapp.repository;
 
+import at.roteskreuz.covidapp.domain.ExportBatch;
 import at.roteskreuz.covidapp.domain.ExportFile;
+import at.roteskreuz.covidapp.model.ExportBatchStatus;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Repository for persisting authorized apps
@@ -12,5 +16,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ExportFileRepository extends CrudRepository<ExportFile, String> {
 
+    List<ExportFile> findAllByBatchIsAndStatusIsNot(ExportBatch batch, ExportBatchStatus status);
 	
 }
