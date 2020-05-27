13c8814675f1510336c6736ba9eed15a1fbd3f21
Bernhard Roessler <bernhard.roessler@accenture.com>
2020-05-27 12:23:28 +0200

    https://tasks.pxp-x.com/browse/CTAA-1485: Backend - Using KeyVault (optional)

---
+++ src/main/java/at/roteskreuz/covidapp/sign/AbstractSign.java
@@ -0,0 +1,33 @@
+package at.roteskreuz.covidapp.sign;
+
+import at.roteskreuz.covidapp.util.PemReader;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.PrivateKey;
+import java.security.Signature;
+
+/**
+ *
+ * @author roesslerb
+ */
+@Slf4j
+public abstract class AbstractSign implements Sign {
+
+	protected byte[] signature(byte[] data, InputStream keyInputStream, String signatureAlgorithm, String keyType)
+									throws GeneralSecurityException, IOException {
+		java.security.Signature ecdsa = Signature.getInstance(signatureAlgorithm);
+		ecdsa.initSign(getPrivateKey(keyInputStream, keyType));
+		ecdsa.update(data);
+		return ecdsa.sign();
+	}
+
+	protected PrivateKey getPrivateKey(InputStream keyInputStream, String keyType)
+			throws IOException, GeneralSecurityException {
+		return PemReader.loadPrivateKey(FileCopyUtils.copyToByteArray(keyInputStream), keyType);
+	}
+
+}
