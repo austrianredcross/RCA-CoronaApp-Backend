ac117d102641ddc9813f2da9f684dada228f9bd2
Puskai Zoltán <zoltan.puskai@gmail.com>
2020-05-27 15:31:04 +0200

    base64 decode

--- src/main/java/at/roteskreuz/covidapp/validation/ExposureKeyValidator.java
+++ src/main/java/at/roteskreuz/covidapp/validation/ExposureKeyValidator.java
@@ -39,8 +39,7 @@ public class ExposureKeyValidator extends AbstractValidator implements Constrain
 		// And have an interval <= maxInterval (configured allowed clock skew)
 		long maxIntervalNumber = now.toInstant(ZoneOffset.UTC).getEpochSecond() /  ApplicationConfig.INTERVAL_LENGTH.getSeconds();
 		
-		//String key =exposureKey.getBinKey();
-		String key =exposureKey.getKey();
+		String key =exposureKey.getBinKey();
 		if (key.length() != ApplicationConfig.KEY_LENGTH) {
 			addErrorMessage(context, "invalid key length, " + key.length() + ", must be " + ApplicationConfig.KEY_LENGTH);
 			result = false;
